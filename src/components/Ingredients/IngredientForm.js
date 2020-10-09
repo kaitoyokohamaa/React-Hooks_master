da3aeea806f1c634f972d3a08a72ea6caf823f58
【kaito yokohama】 <s1A121700885@toyo.jp>
2020-10-09 16:00:29 +0900

    confirm prettier

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -29,7 +29,7 @@ const IngredientForm = React.memo(props => {
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input
+              <input
               type="number"
               id="amount"
               value={enteredAmount}
